ac623bc1c3a7f351b4c0fa1397b24e4d54217a75
Noah <82201000+Nopro263@users.noreply.github.com>
2024-09-20 22:14:53 +0200

    added example code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LehrerIn falki = new LehrerIn("FALKENSTEINER Markus", 40, 'm', 102);
+        LehrerIn groebl = new LehrerIn("GRÖBL Robert", 40, 'm', 103);
+
+        SchuelerIn max = new SchuelerIn("Max Mustermann", 16, 'm', 104);
+        SchuelerIn claudia = new SchuelerIn("Claudia Clementine", 17, 'w', 103);
+
+        max.addFehlstunden(3);
+
+        System.out.printf("Max hat %d Fehlstunden!\n", max.getFehlstunden());
+        System.out.println("Falkensteiner ist in Raum " + falki.getRaum());
     }
 }
